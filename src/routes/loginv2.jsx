f97f0156141b16769ef46ef16a7bcd9a0619b3d7
Marcos Rivas <marcosrivasr@gmail.com>
2022-03-06 14:12:34 -0600

    login v2 created

--- src/routes/loginv2.jsx
+++ src/routes/loginv2.jsx
@@ -11,6 +11,9 @@ import {
 import { onAuthStateChanged } from "firebase/auth";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import AuthProvider from "../components/authProvider";
+import Loading from "../components/loading";
+
+import style from "./login.module.css";
 
 /*
   Stages:
@@ -22,19 +25,47 @@ import AuthProvider from "../components/authProvider";
 */
 export default function LoginV2() {
   const navigate = useNavigate();
+  const [state, setState] = useState(1);
+
+  function handleAuth() {
+    const googleProvider = new GoogleAuthProvider();
+    const signInWithGoogle = async () => {
+      try {
+        const res = await signInWithPopup(auth, googleProvider);
+        if (res) {
+          registerNewUser(res.user);
+        }
+      } catch (err) {
+        console.error(err);
+        //alert(err.message);
+      }
+    };
+    signInWithGoogle();
+  }
+
+  if (state === 4) {
+    return (
+      <div className={style.loginView}>
+        <div>
+          <h1>Link Tree</h1>
+          <button onClick={() => handleAuth()} className={style.provider}>
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <AuthProvider
       onUserLoggedIn={(user) => {
         navigate("/dashboard");
       }}
-      onUserDoesntExist={() => {
-        console.log("usuario no existe");
-      }}
       onUserNotLoggedIn={() => {
-        console.log("Usuario no logueado");
+        setState(4);
       }}
     >
-      <div>Loading</div>
+      <Loading />
     </AuthProvider>
   );
 }
